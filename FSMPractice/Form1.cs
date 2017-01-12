d5dd854ea53e02f74ae29c51aba0aec4e10a2c3a
BlaneTorregrossa <blanet97@cox.net>
2017-01-12 17:58:31 -0600

    TurnBased work

---
+++ FSMPractice/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Combat: 2 Parties: 1 PlayersPerParty: -EndTurn
+//Classes for Combat, Party, and Player
+
+
+namespace FSMPractice
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        // The classes that below are required to have a working turn based system. 
+
+
+        public class Player                        // Class for players 
+        {
+            private int m_characterturn = 1;
+
+
+            public int PlayerTurn
+            {
+                get { return m_characterturn; }
+                set { value = m_characterturn; }
+            }
+
+
+           
+
+        }                                   
+
+        public class Party                         // Class has to contain 2-3 players per party
+        {
+
+
+            private int m_party;
+            private int m_partyturn = 1;
+
+
+            public int PartyTurn
+            {
+                get { return m_partyturn; }
+                set { value = m_partyturn; }
+            }
+
+
+
+            // Players must be used/accessed from here.
+
+            
+
+
+        }
+
+        public class Combat                        // Class has to contain 2 parties 
+        {
+            private int m_turn;
+
+
+            public int Turn
+            {
+                get { return m_turn; }
+                set { value = m_turn; }
+            }
+
+
+            // Parties must be used/accessed from here
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+
+
+    }
+}
--- WinFormPractice/Form1.cs
+++ WinFormPractice/Form1.cs
@@ -12,26 +12,14 @@ namespace WinFormPractice
 {
 
 
-    // EXP needed for new Level is: Current Level multiplied by the Original EXP requirment to Level up (Lvl * Exp = LvlReq).
-    // Button increases Player EXP and as the player Levels up the progress bar resets and the amount of Exp need to level up increases.
-    // Need a counter for current Player Level
-
-
-    class Player : Form1
-    {
-        Player() { }
-        private int m_exp = 0;
-        public int Exp
-        {
-            get { return m_exp; }
-            set { value = m_exp; }
-        }
-
-
-    }
+    // EXP needed for new Level is: Current Level multiplied by the Original EXP requirment to Level up (Lvl * Exp = LvlReq).                   [X]
+    // Button increases Player EXP and as the player Levels up the progress bar resets and the amount of Exp need to level up increases.        [X]
+    // Need a counter for current Player Level                                                                                                  [X]
+    // Have Delegates used                                                                                                                      [ ]
 
 
 
+         
     public partial class Form1 : Form
     {
         public Form1()
@@ -40,41 +28,71 @@ namespace WinFormPractice
         }
 
 
-
-
-
-        private void button1_Click(object sender, EventArgs e)                  // Button labled "button1"
+        private void button1_Click(object sender, EventArgs e)
         {
-            progressBar1.Value += 5;                                            // Increases Progress Bar by 5 if "button1" is pressed.
-            
+            progressBar1.Value += 100;                                          // Increases Progress Bar by 100 if "button1" is pressed.
 
-            if (progressBar1.Value == 100)                                      // If the Progress Bar's value is equal to 100 then Text in the textbox is displayed as "Level Up!" and the Progress Bar's value is set to 0.
+            textBox3.Text = "";                                                 // Clears the text box of previous text that was present in textBox3
+            textBox3.Text += progressBar1.Value;                                // Text Displays the current "Exp" gained by pushing the button.
+
+
+            if (progressBar1.Value == progressBar1.Maximum)                     // If statement that activates if the Value of progressBar1 equals the set maximum given to itself
             {
-                textBox1.Text = "Level Up!!!";
-                progressBar1.Value = 0;      
-                
+                textBox4.Text = "Level Up!!!";                                  // Displays text "Level Up!!!" to textBox4
+                progressBar1.Value = 0;                                         // Resets the value of progressBar4 back to 0
+                progressBar1.Maximum = progressBar1.Maximum * 2;                // Sets the Maximum value allowed for progressBar1 to it's previous value miltiplied by 2
+                progressBar2.Value += 1;                                        // Increases the value of progressBar2 by 1
+                textBox2.Text = "";                                             // Clears the text box of previous text that was present in textBox2
+                textBox2.Text += progressBar2.Value;                            // Displays the value of progressBar2 to textBox2
             }
 
-            
-        }
+            if (progressBar2.Value == progressBar2.Maximum)                     // If statement that activates if the value of progressBar2 is equal to the maximum set for itself
+            {
+                textBox4.Text = "Max Level!";                                   // Displays "Max Level!" to textBox4
+                button1.Enabled = false;                                        // Disables button1 to prevent the addition of more "Exp"
+            }
 
-        private void button2_Click(object sender, EventArgs e)                  // Button labled "button2"
-        {
-            progressBar1.Value -= 5;                                           // Decreases Progress Bar by 5 if "button2" is pressed.
-        }
-
-     
-
-        private void progressBar1_Click(object sender, EventArgs e)             // Progress Bar
-        {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)           // Text Box
+
+
+
+        private void progressBar1_Click(object sender, EventArgs e)
         {
 
         }
 
-       
+        private void progressBar2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
